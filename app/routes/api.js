59ed62ea5604f6bced2a90493833e3400ebb9c5f
Nipe Setiawan <nipeharefa@gmail.com>
2016-11-28 10:50:55 +0700

    fix space

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,8 +10,9 @@ const mongoUrl = process.env.MONGODB_URL
 
 mongoose.Promise = global.Promise
 
-router.post('/api/product', ProductCtrl.getUser)
-
 mongoose.connect(mongoUrl);
+
+
+router.post('/api/product', ProductCtrl.getUser)
 
 module.exports  = router
