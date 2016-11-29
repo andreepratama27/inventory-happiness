705471119878d8d9611303951cdc5d1dee315535
Nipe Setiawan <nipeharefa@gmail.com>
2016-11-29 15:37:55 +0700

    add travis env

--- test/test.js
+++ test/test.js
@@ -8,11 +8,22 @@ var app = express();
 app.use(webRouter)
 app.set('views', './views')
 app.set('view engine', 'ejs')
+console.log(process.env.DBTRAVIS)
 
-describe('GET /user', function() {
+describe('GET Web Route', function() {
   it('get index page', function(done) {
     request(app)
       .get('/')
       .expect(200, done);
-  });
+  })
+  it('get login page', function(done){
+  	request(app)
+      .get('/login')
+      .expect(200, done);
+  })
+  it('get hoax page', function(done){
+  	request(app)
+      .get('/hoaxer')
+      .expect(404, done);
+  })
 });
